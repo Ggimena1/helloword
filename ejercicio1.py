fb0578307d39442c090405b426a09a3d88cd02c5
Ggimena1 <158097650+Ggimena1@users.noreply.github.com>
2024-02-05 15:01:44 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+import altair as alt
+import pandas as pd
+import numpy as np
+
+st.write('LAVADOS')
+st.header('st.button')
+if st.button ('Hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
+st.header('st.write')
+# ejemplo 1
+st.write('HELLO, *CONSULTA DE LAVADOS!* :sunglasses:')
+#ejemplo 2
+st.write(1234)
+
+#ejemplo 3
+df = pd.DataFrame ({
+    'first column':[1, 2, 3, 4],
+    'second column':[10, 20, 30, 40]
+    })
+st.write(df)
+
+#ejemplo 4
+st.write('Below is a DataFreme:', df, 'above is a datafreme.')
+
+#ejemplo 5 muestra graficos y se representan en circulos
+df2 = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a','b','c'])
+c = alt.Chart(df2).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b','c'])
+st.write(c)
