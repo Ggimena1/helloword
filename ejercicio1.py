79de971e6bf063691ba0537e29d8eecd9a2ca082
Ggimena1 <158097650+Ggimena1@users.noreply.github.com>
2024-02-05 15:17:02 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -30,5 +30,5 @@ df2 = pd.DataFrame(
     np.random.randn(200, 3),
     columns=['a','b','c'])
 c = alt.Chart(df2).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b','c'])
+    x='a', y='b', size='c', color='b', tooltip=['a', 'b','c'])
 st.write(c)
